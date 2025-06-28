5f081d35b4208064f2279aac4e8ccb74aff303d5
Nidhi Yellamraju <nidhiyellamraju@MacBookPro.attlocal.net>
2025-06-27 19:01:18 -0700

    fixrouting for github

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/home';
 import CriminalLaw from './routes/CriminalLaw';
 import CivilLaw from './routes/CivilLaw';
